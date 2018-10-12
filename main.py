c1d67e23dd900f124fbe56eb14c6ebe4c0f1e14e
Alexander Teno <alexander.teno@gmail.com>
2018-10-12 15:54:18 -0400

    Simple damage calculation

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 
 from enum import Enum
 from collections import namedtuple
-from functools import reduce
 
 Status = namedtuple('Status', 'status_effect duration')
 
 
 class StatusEffect(Enum):
     SLOWED = 0
+
+
+class EnemyType(Enum):
+    HERO = 0
+    MINION = 1
 
 
 class RaynorTalent(Enum):
@@ -19,8 +23,14 @@ class RaynorTalent(Enum):
 
 
 class Enemy:
-    def __init__(self):
+    def __init__(self, enemy_type):
         self.__statuses = []
+        self.__enemy_type = enemy_type
+        self.__damage = 0
+
+    def __repr__(self):
+        return 'Enemy(statuses={},enemy_type={},damage={})'.format(
+            self.__statuses, self.__enemy_type, self.__damage)
 
     def apply_status_effect(self, status_effect, duration):
         self.__statuses.append(Status(status_effect, duration))
@@ -30,6 +40,9 @@ class Enemy:
             if status.status_effect is status_effect:
                 return True
         return False
+
+    def is_hero(self):
+        return self.__enemy_type is EnemyType.HERO
 
     def add_damage(self, amount):
         self.__damage += amount
@@ -49,7 +62,15 @@ class Raynor:
     def __init__(self, level, talents):
         self.__level = level
         self.__talents = talents
-        self.__attack_count = 0
+        self.__pepper_counter = 0
+        self.__veteran_marksman_count = 0
+
+    def __repr__(self):
+        return (
+            'Raynor(level={},talents={},veteran_marksman_count={},pepper_count'
+            'er={})').format(self.__level, self.__talents,
+                             self.__veteran_marksman_count,
+                             self.__pepper_counter)
 
     def has_veteran_marksman(self):
         return RaynorTalent.VETERAN_MARKSMAN in self.__talents
@@ -60,20 +81,41 @@ class Raynor:
     def has_raynors_raider(self):
         return RaynorTalent.RAYNORS_RAIDER in self.__talents
 
-    def give_em_same_pepper(self):
+    def attack_will_pepper(self):
         return (self.attack_count + 1 % 4) == 0
 
-    def calculate_damage(self, target):
-        pepper_damage = 0 if self.give_em_some_pepper() else 0
+    def increment_pepper_counter(self, target):
+        self.__pepper_counter = (self.__pepper_counter + 1) % 4
+        if self.__pepper_counter == 0 and self.has_veteran_marksman() and \
+            target.is_hero():
+            self.__veteran_marksman_count += 1
+
+    def get_pepper_damage(self):
+        if self.__pepper_counter != 0:
+            return 0
+        pepper_modifier = Raynor.GIVE_EM_SOME_PEPPER + \
+            self.__veteran_marksman_count * Raynor.VETERAN_MARKSMAN
+        return Raynor.BASE_ATTACK * pepper_modifier
+
+    def attack(self, target):
+        self.increment_pepper_counter(target)
+        target.add_damage(Raynor.BASE_ATTACK + self.get_pepper_damage())
 
 
 def main():
 
-    # vm_raynor_1 = Raynor(1, [RaynorTalent.VETERAN_MARKSMAN])
-    enemy = Enemy()
-    print(enemy.has_status(StatusEffect.SLOWED))
-    enemy.apply_status_effect(StatusEffect.SLOWED, 1)
-    print(enemy.has_status(StatusEffect.SLOWED))
+    vm_raynor_1 = Raynor(1, [RaynorTalent.VETERAN_MARKSMAN])
+    aith_raynor_1 = Raynor(1, [RaynorTalent.ACE_IN_THE_HOLE])
+
+    vm_enemy_1 = Enemy(EnemyType.HERO)
+    aith_enemy_1 = Enemy(EnemyType.HERO)
+    for _ in range(30):
+        vm_raynor_1.attack(vm_enemy_1)
+        aith_raynor_1.attack(aith_enemy_1)
+    print(vm_raynor_1)
+    print(vm_enemy_1)
+    print(aith_raynor_1)
+    print(aith_enemy_1)
 
 
 if __name__ == '__main__':
